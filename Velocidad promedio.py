979c6537a0cc2815b71c6da1a57c284a51b96638
A01748918 <46898102+A01748918@users.noreply.github.com>
2019-01-21 09:40:24 -0600

    César Guzmán Guadarrama, Grupo 02.

---
+++ Velocidad promedio.py
@@ -0,0 +1,7 @@
+#Autor: César Guzmán Guadarrama A01748918
+#Calcula la velocidad promedio en un viaje
+
+tiempo = float(input("Dame el tiempo de viaje"))
+distancia = float(input("Dame la distancia"))
+velocidad = distancia/tiempo
+print("Tu velocidad es de", velocidad, "Km/h")
